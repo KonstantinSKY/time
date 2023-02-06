e290cec77b8344f3baa77a107db54c5556ff29fb
SKY <sky012877@gmail.com>
2023-02-06 14:18:37 -0800

    rnd_sleep_timer.py - Modified :

--- rnd_sleep_timer.py
+++ rnd_sleep_timer.py
@@ -1,6 +1,11 @@
 #!/bin/pyton
 
 import time
+from random import randint,  random, uniform
+import keyboard
+
+STOP_KEY = False
+
 
 def countdown(t):
     while t:
@@ -11,12 +16,34 @@ def countdown(t):
         t -= 1
 
 
+def _return():
+    global STOP_KEY
+    print("Module stopped")
+    STOP_KEY = True
+    return
 
+
+def rnd_sleep(start, finish):
+    delay = uniform(start, finish)
+    print("Delay for :  ", delay, " sec")
+
+    try:
+        print("Push Ctrl + Shift + j  for stop delay and exit function")
+        keyboard.add_hotkey("ctrl+shift+j", lambda: _return())
+    except Exception:
+        print("For STOP_KEY you need to run in root ")
+
+    while delay >= 1:
+        if STOP_KEY:
+            return
+        time.sleep(1)
+        delay -= 1
+        print(delay, end='\r')
 
 
 if __name__ == "__main__":
+    rnd_sleep(10, 19)
 
+#t = input("Enter the time in seconds: ")
 
-t = input("Enter the time in seconds: ")
-
-countdown(int(t))
+#countdown(int(t))
